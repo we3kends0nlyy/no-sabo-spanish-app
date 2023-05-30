f5e58c68aa883b561104f74dcc4fca5ce12753f2
Julian Aparicio <japaric4@uci.edu>
2023-05-30 16:47:52 -0700

    Choosing a random option
    I created a function called generate_random_number in my Rule class which has a way of accounting for the weights of the options in a grammar and choosing a random option every time. I tested this manually to make sure it works and it does. I have no unit tests yet because I'm going to get a couple more things set up in my code before I make unit testing for it.

--- project4.py
+++ project4.py
@@ -34,12 +34,11 @@ class Recursion:
         self.current_class = current_class
         self.previous_class = previous_class
     def recursive_function(self, current_sentence, next_class):
-        print(current_sentence)
-        quit()
         #if no terminals:
             #print(current_sentence)
         #else:
-            #current_class.generate_sentence_fragment(current_sentence, next_class)
+            #next_class.generate_sentence_fragment(current_sentence, next_class)
+        next_class.generate_sentence_fragment(current_sentence, next_class)
 
 
 '''
@@ -61,20 +60,34 @@ class Option:
     def generate_sentence_fragment():
         pass
 '''
+
 class Rule:
     def __init__(self):
         pass
 
-    def generate_random_number(self):
-        pass
+    def generate_random_number(self, options_of_start_variable):
+        list_of_weights = []
+        for option in options_of_start_variable:
+            for i in range(int(option[0])):
+                list_of_weights.append(option[0])
+        random_num_of_list_of_weights = random.choice(list_of_weights)
+        while True:
+            random_num_in_ops_of_start_var = random.randint(0, len(options_of_start_variable)-1)
+            if options_of_start_variable[random_num_in_ops_of_start_var][0] == random_num_of_list_of_weights:
+                final_option_chosen = options_of_start_variable[random_num_in_ops_of_start_var]
+                break
+        return final_option_chosen
+
+
     #go through the current sentence fragment, which at this point
     #will a list with lists inside of it, each sublist containing,
     #an option. so go through that list and add up all the first
     ##later.......
 
-    def generate_sentence_fragment(self, options_of_start_variable):
-        pass
-        #generate_random_number(options_of_start_variable)
+    def generate_sentence_fragment(self, options_of_start_variable, current_class):
+        option_chosen = self.generate_random_number(options_of_start_variable)
+
+
 
 
 
@@ -94,6 +107,7 @@ class Grammar:
                         options_of_search_variable.append(next_line.strip("\n").split(" "))
         except StopIteration:
             pass
+        del(options_of_search_variable[-1])
         rule = Rule()
         recurse = Recursion()
         recurse.recursive_function(options_of_search_variable, rule)
