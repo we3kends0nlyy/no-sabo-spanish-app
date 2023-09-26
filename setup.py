b7e5f43c71007c5d14771305c0158b725151f616
Julian Aparicio <japaric4@uci.edu>
2023-09-26 02:23:03 -0700

    commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='no-sabo-spanish-app',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'flask',
+    ],
+)
